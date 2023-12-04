577f19508f4e9dad02d216cd4b9cb83880d76ebf
Guilherme Henrique <guilherme.serafini@sou.unifal-mg.edu.br>
2023-12-04 10:22:05 -0300

    att front 2

--- journeymapspike-front/src/index.js
+++ journeymapspike-front/src/index.js
@@ -3,10 +3,6 @@ import { createRoot } from "react-dom/client";
 import { Stage, Layer, Rect, Circle, Text, Group } from "react-konva";
 import axios from "axios";
 import "./index.css";
-import { VscSave, VscSettingsGear } from 'react-icons/vsc';
-import { BiSolidHelpCircle, BiSolidLogIn } from 'react-icons/bi';
-import { GrDocumentConfig } from 'react-icons/gr';
-import { IoIosAddCircle } from "react-icons/io";
 import ReactDOM from "react-dom";
 import Matrix from './components/Matrix';
 import NavBar from './components/NavBar';
@@ -468,59 +464,10 @@ const App = () => {
   };
 
 
-  const Navbar = ({ setActivePhase }) => {
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [selectedMap, setSelectedMap] = useState("Mapa 1");
-
-    const handleMascoteClick = () => {
-      console.log("Mascote clicado!");
-    };
-
-    const handleOptionClick = (option) => {
-      console.log(`${option} clicado!`);
-    };
-
-    const handleMapChange = (event) => {
-      setSelectedMap(event.target.value);
-      // mudar o mapa
-    };
-
-    return (
-      <nav className="navbar">
-        <div className="logo" onClick={handleMascoteClick}>
-          <img src={process.env.PUBLIC_URL + '/Mascote.svg'} alt="Mascote Logo" className="mascoteperso" />
-        </div>
-        <div className="options">
-          <div className="dropdown-container">
-            <label htmlFor="mapDropdown">Mapa: </label>
-            <select id="mapDropdown" value={selectedMap} onChange={handleMapChange}>
-              <option value="Mapa 1">Mapa 1</option>
-              <option value="Mapa 2">Mapa 2</option>
-            </select>
-          </div>
-          <div className="botaosalvar">
-            <button className="arrumarbotao" id="" onClick={() => { handleSaveClick(); showMascote(); }}>
-              <VscSave className="icons2" />
-              <div className="textoarrumar">Salvar</div>
-            </button>
-          </div>
-          <button className="option-btn" onClick={() => handleOptionClick("Login")}>
-            <BiSolidLogIn className="icons" />
-          </button>
-          <button className="option-btn" onClick={() => handleOptionClick("Configurações")}>
-            <VscSettingsGear className="icons" />
-          </button>
-          <button className="option-btn" onClick={() => handleOptionClick("Ajuda")}>
-            <BiSolidHelpCircle className="icons" />
-          </button>
-        </div>
-      </nav>
-    );
-  };
 
   return (
     <div>
-      <Navbar />
+      <NavBar />
       <div className="map-container">
         <Stage width={window.innerWidth} height={window.innerHeight}>
           <Layer>
