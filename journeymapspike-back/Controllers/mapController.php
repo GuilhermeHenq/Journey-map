0e707f186ac4469c9dc6c3239d12699572a79539
Guilherme Henrique Pereira Serafini <108468458+GuilhermeHenq@users.noreply.github.com>
2024-01-08 20:15:37 -0300

    Delete journeymapspike-back directory

--- journeymapspike-back/Controllers/mapController.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../Model/mapModel.php';
-
-class MapController {
-    public function getAllItems() {
-        $mapModel = new MapModel();
-        $mapModel->getAllItems();
-    }
-}
